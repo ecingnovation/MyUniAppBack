9ce99a1ffedd7249cbc689daf39139e6a8390557
Oscar Pinto <moka117@hotmail.com>
2019-03-26 15:01:19 -0500

    First Commit

---
+++ src/main/java/com/MyUniApp/MyUniAppBack/MyUniAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.MyUniApp.MyUniAppBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUniAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUniAppBackApplication.class, args);
+	}
+
+}
