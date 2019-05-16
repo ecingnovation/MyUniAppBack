5fc86cecbb8bfc0c41da36138902e1a6343b4991
Oscar Pinto <36512466+TheBaphomet666@users.noreply.github.com>
2019-05-16 10:54:26 -0500

    Update KioskoController.java

--- src/main/java/com/MyUniApp/MyUniAppBack/Controller/KioskoController.java
+++ src/main/java/com/MyUniApp/MyUniAppBack/Controller/KioskoController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000", "https://myuniapp-eci.firebaseapp.com"})
+//@CrossOrigin(origins = {"http://localhost:3000", "https://myuniapp-eci.firebaseapp.com"})
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/kioskos")
 public class KioskoController {
