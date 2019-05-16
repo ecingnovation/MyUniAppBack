6d73074527755faa8321ef324c68555eb8971286
Oscar Pinto <36512466+TheBaphomet666@users.noreply.github.com>
2019-05-16 09:53:10 -0500

    Update SwaggerConfig.java

--- src/main/java/com/MyUniApp/MyUniAppBack/Config/SwaggerConfig.java
+++ src/main/java/com/MyUniApp/MyUniAppBack/Config/SwaggerConfig.java
@@ -7,10 +7,22 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import com.google.common.base.Predicates;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+     private static final String TITLE = "Myuniapp API";
+
+    /**
+     * The API Description
+     */
+    private static final String DESCRIPTION = "Myuniapp API";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -21,5 +33,18 @@ public class SwaggerConfig {
                     .paths(PathSelectors.any())
                     .build()
                 .pathMapping("/");
+    }
+        /**
+     * The API information to be presented on the Swagger
+     * @return the api info
+     * @param version the application version
+     */
+    private ApiInfo apiInfo(final String version) {
+
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version(version)
+                .build();
     }
 }
