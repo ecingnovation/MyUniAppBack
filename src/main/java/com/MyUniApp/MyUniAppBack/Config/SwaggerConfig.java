d001071f13e18aead8c6a9a28e7fa904736d5b1c
Oscar Pinto <36512466+TheBaphomet666@users.noreply.github.com>
2019-05-16 09:47:53 -0500

    Update SwaggerConfig.java

--- src/main/java/com/MyUniApp/MyUniAppBack/Config/SwaggerConfig.java
+++ src/main/java/com/MyUniApp/MyUniAppBack/Config/SwaggerConfig.java
@@ -14,9 +14,12 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                  .select()
+                     .apis(Predicates.and(
+                            RequestHandlerSelectors.withClassAnnotation(RestController.class),
+                            RequestHandlerSelectors.basePackage("com.MyUniApp")))
+                    .paths(PathSelectors.any())
+                    .build()
+                .pathMapping("/");
     }
 }
