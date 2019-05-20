f913e8b4603b934615914e6cef54110ad3a186b0
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2019-05-19 23:48:07 -0500

    Added DELETE method to News

--- src/main/java/com/MyUniApp/MyUniAppBack/Repositories/NewsRepository.java
+++ src/main/java/com/MyUniApp/MyUniAppBack/Repositories/NewsRepository.java
@@ -3,5 +3,5 @@ package com.MyUniApp.MyUniAppBack.Repositories;
 import com.MyUniApp.MyUniAppBack.Model.AppNew;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NewsRepository extends MongoRepository<AppNew,String> {
+public interface NewsRepository extends MongoRepository<AppNew,Integer> {
 }
